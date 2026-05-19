350f04585b4859ffd0c7682684b42d2dd9a21cc5
Anna Louro <annab.louro@gmail.com>
2026-05-18 22:56:06 -0300

    Update fluxoCabelo.js

--- backend/src/flows/fluxoCabelo.js
+++ backend/src/flows/fluxoCabelo.js
@@ -1,4 +1,5 @@
 import { responder } from "../services/chatbotService.js";
+import { adicionarServico } from "../services/adicionarServicoService.js";
 import { fluxoPrincipal } from "./fluxoPrincipal.js";
 import { isMenuRequest } from "./fluxoVoltarMenu.js";
 
@@ -11,26 +12,47 @@ export function fluxoCabelo(
         return responder(usuario, "oi");
     }
     switch (mensagem) {
+       
         case "1":
-            usuario.servico = "Corte Simples";
+            adicionarServico(
+                usuario,
+                "Corte Simples",
+                35
+            );
             usuario.etapa = "pedir.nome";
 
             return `Ótima escolha!\nAgora, para finalizarmos o agendamento me diga seu nome (digite apenas seu nome)\n\n(Digite 'menu' para voltar ao menu principal)`;
+       
         case "2":
-            usuario.servico = "Corte com acabamento";
+            adicionarServico(
+                usuario,
+                "Corte com acabamento",
+                45
+            );
             usuario.etapa = "pedir.nome";
 
             return `Boa escolha!\nAgora, para finalizarmos o agendamento me diga seu nome (digite apenas seu nome)\n\n(Digite 'menu' para voltar ao menu principal)`;
+       
         case "3":
-            usuario.servico = "Corte Degradê";
+            adicionarServico(
+                usuario,
+                "Corte Degradê",
+                50
+            );
             usuario.etapa = "pedir.nome";
 
             return `Escolha incrível!\nAgora, para finalizarmos o agendamento me diga seu nome (digite apenas seu nome)\n\n(Digite 'menu' para voltar ao menu principal)`;
+       
         case "4":
-            usuario.servico = "Corte Social";
+            adicionarServico(
+                usuario,
+                "Corte Social",
+                40
+            );
             usuario.etapa = "pedir.nome";
 
             return `Ótima escolha!\nAgora, para finalizarmos o agendamento me diga seu nome (digite apenas seu nome)\n\n(Digite 'menu' para voltar ao menu principal)`;
+        
         case "5":
             return `A duração média dos cortes é de 50 min.\nNossos profissionais são:\nGuilherme Dino\nCaio Rosa\nSamuel Goes\nTodos com mais de 5 anos de experiência em todos os tipos de corte. Nossos produtos são da maior qualidade, como máquinas da Wahl e Rovra.\n\n(Digite 'menu' para voltar ao menu principal)`;
         default:
